8568ce8d11d0aa7dc2bfc04f3a83ca577b9e7055
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-08-22 11:58:24 +0100

    add wallpapers

--- dot_config/waybar/scripts/executable_theme-sed-toggle.sh
+++ dot_config/waybar/scripts/executable_theme-sed-toggle.sh
@@ -1,5 +1,4 @@
-!/bin/bash
-
+#!/bin/bash
 # Orange Theme Toggle using sed to replace CSS import
 #
 # HOW IT WORKS:
@@ -13,7 +12,6 @@
 # - Uses ~/.config/waybar/.theme-state as state file
 # - File contains: theme=nord or theme=orange (extensible for more themes)
 # - Default is nord if file doesn't exist
-
 WAYBAR_DIR="$HOME/.config/waybar"
 STYLE_FILE="$WAYBAR_DIR/style.css"
 STATE_FILE="$WAYBAR_DIR/.theme-state"
@@ -31,21 +29,21 @@ fi
 if [[ "$CURRENT_THEME" == "nord" ]]; then
     # Currently nord, switch to orange
     sed -i 's|@import url("./nord-theme.css");|@import url("./orange-theme.css");|g' "$STYLE_FILE"
-    sed -i 's|~/Downloads/wallpapers/.*|~/Downloads/wallpapers/fallout.jpg|g' "$HYPRPAPER_FILE"
+    sed -i 's|~/.config/wallpapers/.*|~/.config/wallpapers/fallout.jpg|g' "$HYPRPAPER_FILE"
     sed -i 's|include ./nord-theme.conf|include ./orange-theme.conf|g' "$KITTY_CONFIG"
     echo "theme=orange" > "$STATE_FILE"
     echo "Switched to Orange theme"
 elif [[ "$CURRENT_THEME" == "orange" ]]; then
     # Currently orange, switch to scooz
     sed -i 's|@import url("./orange-theme.css");|@import url("./pink-theme.css");|g' "$STYLE_FILE"
-    sed -i 's|~/Downloads/wallpapers/.*|~/Downloads/wallpapers/anime.png|g' "$HYPRPAPER_FILE"
+    sed -i 's|~/.config/wallpapers/.*|~/.config/wallpapers/anime.png|g' "$HYPRPAPER_FILE"
     sed -i 's|include ./orange-theme.conf|include ./pink-theme.conf|g' "$KITTY_CONFIG"
     echo "theme=scooz" > "$STATE_FILE"
     echo "Switched to Scooz theme"
 else
     # Currently scooz (or unknown), switch to nord
     sed -i 's|@import url("./pink-theme.css");|@import url("./nord-theme.css");|g' "$STYLE_FILE"
-    sed -i 's|~/Downloads/wallpapers/.*|~/Downloads/wallpapers/city2.png|g' "$HYPRPAPER_FILE"
+    sed -i 's|~/.config/wallpapers/.*|~/.config/wallpapers/hollow-sat.jpg|g' "$HYPRPAPER_FILE"
     sed -i 's|include ./pink-theme.conf|include ./nord-theme.conf|g' "$KITTY_CONFIG"
     echo "theme=nord" > "$STATE_FILE"
     echo "Switched to Nord theme"
