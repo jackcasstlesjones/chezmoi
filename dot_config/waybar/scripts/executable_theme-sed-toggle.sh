488985d8ffc6a2c98079d97af80efce930027810
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-08-23 13:59:47 +0100

    pywal

--- dot_config/waybar/scripts/executable_theme-sed-toggle.sh
+++ dot_config/waybar/scripts/executable_theme-sed-toggle.sh
@@ -2,23 +2,25 @@
 
 # Theme arrays
 THEMES=("nord-theme.css" "orange-theme.css" "pink-theme.css" "nord-theme.css" "nord-theme.css")
-WALLPAPERS=("city2.png" "fallout.jpg" "anime.png" "hollow-sat.jpg" "hollow-yellow.png")  
-KITTY_THEMES=("nord-theme.conf" "orange-theme.conf" "pink-theme.conf" "nord-theme.conf" "nord-theme.conf")
+WALLPAPERS=("city2.png" "fallout.jpg" "anime.png" "hollow-sat.jpg" "hollow-yellow.png")
 
 # Config files
 STYLE_FILE="$HOME/.config/waybar/style.css"
 STATE_FILE="$HOME/.config/waybar/.theme-state"
 HYPRPAPER_FILE="$HOME/.config/hypr/hyprpaper.conf"
-KITTY_CONFIG="$HOME/.config/kitty/kitty.conf"
 
 # Get current index
 CURRENT_INDEX=$(cat "$STATE_FILE" 2>/dev/null || echo 0)
 NEXT_INDEX=$(((CURRENT_INDEX + 1) % ${#THEMES[@]}))
 
-# Apply theme
+# Apply waybar theme
 sed -i "s|@import url(\"./.*-theme.css\");|@import url(\"./${THEMES[$NEXT_INDEX]}\");|g" "$STYLE_FILE"
+
+# Update hyprpaper config
 sed -i "s|~/.config/wallpapers/.*|~/.config/wallpapers/${WALLPAPERS[$NEXT_INDEX]}|g" "$HYPRPAPER_FILE"
-sed -i "s|include ./.*-theme.conf|include ./${KITTY_THEMES[$NEXT_INDEX]}|g" "$KITTY_CONFIG"
+
+# Use pywal for terminal colors (skip wallpaper since hyprpaper handles it)
+wal -i "$HOME/.config/wallpapers/${WALLPAPERS[$NEXT_INDEX]}" -n -q
 
 # Save new index
 echo "$NEXT_INDEX" > "$STATE_FILE"
@@ -26,4 +28,3 @@ echo "$NEXT_INDEX" > "$STATE_FILE"
 # Reload
 pkill -SIGUSR2 waybar
 pkill hyprpaper && hyprpaper &
-kitty @ load-config
