d4e66619d8127cf942f9932960eea61ad0c24cac
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-09-15 21:39:42 +0100

    hollow knight background

--- dot_config/waybar/scripts/executable_theme-sed-toggle.sh
+++ dot_config/waybar/scripts/executable_theme-sed-toggle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wallpaper array
-WALLPAPERS=("city2.png" "fallout.jpg" "anime.png" "hollow-sat.jpg" "hollow-yellow.png" "herakles.png" "frosted-dark.png" "nord-mountain.png")
+WALLPAPERS=("city2.png" "fallout.jpg" "anime.png" "hollow-sat.jpg" "hollow-yellow.png" "herakles.png" "frosted-dark.png" "nord-mountain.png" "hollowknight-cartoon.jpg")
 
 # Config files
 STYLE_FILE="$HOME/.config/waybar/style.css"
