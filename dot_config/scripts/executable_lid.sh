56d1bf87db01937d6a9adffa148d3efc90cde1ab
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2026-04-16 09:25:37 +0100

    lid executable with t14m monitor

--- dot_config/scripts/executable_lid.sh
+++ dot_config/scripts/executable_lid.sh
@@ -5,7 +5,7 @@ if [[ $1 == "open" ]]; then
   hyprctl keyword monitor "desc:Lenovo Group Limited NE140WUM-N6M,1920x1200@60,2887x0,1"
 else
   # Only disable laptop screen when lid closes if external monitor is connected
-  if hyprctl monitors -j | jq -e '.[] | select(.description | contains("Samsung") or contains("Dell"))' > /dev/null; then
+  if hyprctl monitors -j | jq -e '.[] | select(.description | contains("Samsung") or contains("Dell") or contains("T24m"))' > /dev/null; then
     hyprctl keyword monitor "desc:Lenovo Group Limited NE140WUM-N6M,disable"
   fi
 fi
